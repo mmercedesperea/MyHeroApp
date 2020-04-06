3c4efadcb0f67b1d8fc9838f988a082fe1649504
Merche <mdem.perea@gmail.com>
2020-04-06 12:47:04 +0200

    user routes

--- backend/src/database/db.js
+++ backend/src/database/db.js
@@ -27,14 +27,13 @@
 
 
 let _db = null
-let pruebab =null;
 
 const mysql = require('mysql');
 
 class DBConexion {
-    constructor(){
+    constructor() {
         pruebabd: ""
-        
+
     }
 
     async conexion(req, res) {
@@ -57,34 +56,40 @@ class DBConexion {
             });
 
         _db = mysqlConnection
+        global.mysqlConnection = mysqlConnection;
     }
 
     async consulta(sql) {
-        // const mysqlConnection = mysql.createConnection({
-        //     host: 'localhost',
-        //     port: '3306',
-        //     user: 'root',
-        //     password: '',
-        //     database: 'myheroapp'
 
-        // });
+            return new Promise((resolve, reject) => {
+                _db.query(sql,
+                    (err, rows, fields) => {
+                        if (err) console.log(err);
+                        // _row = rows
+                        // console.log(rows)
+                        resolve(rows);
+                    })
+            
 
-        // // function DBStart() {
-        // mysqlConnection
-        //     .connect((err) => {
-        //         if (err) throw err;
-        //         console.log("DB is connected");
+        })
 
-        //         // }
-        //     });
-        return await _db.query(sql, (err, rows) => {
-            if (err) console.log(err);
-            console.log(rows)
-            // res.json(rows)
-            return rows
-           
-        });
+        
     }
+
+// global.mysqlConnection = mysqlConnection;
+    // async dbData(sql) {
+
+    //     const resultado= await _db.query(sql, (err, rows) => {
+    //         if (err) console.log(err);
+    //         // console.log(rows)
+    //         // res.json(rows)
+    //         return JSON.stringify(rows);
+
+    //     });
+
+    //     console.log(resultado);
+
+    // }
     // }
 
 
